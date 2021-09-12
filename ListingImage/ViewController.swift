88646dd4af09c2de50650323e87a1bce8c8e29d7
P <phatnguyen876@gmail.com>
2021-09-12 15:32:58 +0700

    Show List Image Name to screen

--- ListingImage/ViewController.swift
+++ ListingImage/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
         // Do any additional setup after loading the view.
+        listingImage()
+    }
+    
+    func listingImage() {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("img") {
+                // add to List
+                pictures.append(item)
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        // Style
+        cell.textLabel?.text = pictures[indexPath.row]
+        cell.textLabel?.textColor = .black
+        cell.backgroundColor = .white
+
+        return cell
+    }
 
 }
 
