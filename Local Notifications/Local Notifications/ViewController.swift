57c1034c978191e592dc498d0a23b34321ce8109
P <phatnguyen876@gmail.com>
2021-12-26 17:28:52 +0700

    Hacking with Swift

---
+++ Local Notifications/Local Notifications/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Local Notifications
+//
+//  Created by Phat Nguyen on 26/12/2021.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navBar()
+    }
+    
+    func navBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "schedule", style: .plain, target: self, action: #selector(scheduleLocal))
+    }
+
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Yeee")
+            } else {
+                print("Ohhhh")
+            }
+        }
+    }
+
+    @objc func scheduleLocal() {
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Wake up"
+        content.body = "A new day, new creatives"
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = UNNotificationSound.default
+        
+        // set Time to alert
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+        // let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
+}
+
