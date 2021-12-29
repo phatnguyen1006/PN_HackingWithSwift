1213705199e1f8b711188456f266ff60451f702a
P <phatnguyen876@gmail.com>
2021-12-29 23:14:13 +0700

    unknow btn

--- Detect A Beacon/Detect A Beacon/ViewController.swift
+++ Detect A Beacon/Detect A Beacon/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var distanceReading: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
