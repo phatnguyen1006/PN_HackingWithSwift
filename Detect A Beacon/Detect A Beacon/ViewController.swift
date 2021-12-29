82304d47d10350ecf1304621b2bdcd02d46adf2f
P <phatnguyen876@gmail.com>
2021-12-29 23:38:45 +0700

    update extension

--- Detect A Beacon/Detect A Beacon/ViewController.swift
+++ Detect A Beacon/Detect A Beacon/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController {
     @IBOutlet var distanceReading: UILabel!
     var locationManager: CLLocationManager?
     
@@ -35,3 +35,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if manager.authorizationStatus == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    
+                }
+            }
+        }
+    }
+}
