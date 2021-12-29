6ac4fc98f0c7e672856ac7d7543133d16b931ab3
P <phatnguyen876@gmail.com>
2021-12-29 23:08:03 +0700

    update info.plist

--- Detect A Beacon/Detect A Beacon/ViewController.swift
+++ Detect A Beacon/Detect A Beacon/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    /**
+        1. If you request Always access, users will still get the chance to choose When In Use.
+        2. If they choose Always, iOS will automatically ask them again after a few days to confirm they still want to grant Always access.
+        3. When your app is using location data in the background the iOS UI will update to reflect that – users will know it’s happening.
+        4. Users can, at any point, go into the settings app and change from Always down to When In Use.
+    */
+    
+    
 }
 
