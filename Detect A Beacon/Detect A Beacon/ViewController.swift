2fb11bdf0f77732a73799094896dedd05108bb2e
P <phatnguyen876@gmail.com>
2021-12-29 23:19:00 +0700

    checkout

--- Detect A Beacon/Detect A Beacon/ViewController.swift
+++ Detect A Beacon/Detect A Beacon/ViewController.swift
@@ -8,13 +8,15 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet var distanceReading: UILabel!
     var locationManager: CLLocationManager?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .gray
+        initialLocationManager()
     }
 
     /**
@@ -23,6 +25,12 @@ class ViewController: UIViewController {
         3. When your app is using location data in the background the iOS UI will update to reflect that – users will know it’s happening.
         4. Users can, at any point, go into the settings app and change from Always down to When In Use.
     */
+    
+    func initialLocationManager() {
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+    }
     
     
 }
