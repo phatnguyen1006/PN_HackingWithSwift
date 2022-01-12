6b2e13f23e293b4775620bdcb58b3a35faa5cde8
P <phatnguyen876@gmail.com>
2022-01-12 14:55:42 +0700

    secret

--- Secret Swift/Secret Swift/ViewController.swift
+++ Secret Swift/Secret Swift/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
     @IBOutlet var secret: UITextView!
@@ -13,6 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Nothing to see here"
         setupNotificationCenter()
     }
     
@@ -21,9 +23,26 @@ class ViewController: UIViewController {
         
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
+        notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)
     }
-
-    @IBAction func authenticateTapped(_ sender: Any) {
+    
+    func unlockSecretMessage() {
+        // show content
+        secret.isHidden = false
+        title = "Secret"
+        
+        secret.text = KeychainWrapper.standard.string(forKey: "SecretMessage") ?? ""
+    }
+    
+    @objc func saveSecretMessage() {
+        guard secret.isHidden == false else { return }
+        
+        KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
+        secret.isHidden = true
+        secret.resignFirstResponder()
+        title = "Nothing to see here"
+        
     }
     
     @objc func adjustForKeyboard(notication: Notification) {
@@ -42,6 +61,39 @@ class ViewController: UIViewController {
         
         let selectedRange = secret.selectedRange
         secret.scrollRangeToVisible(selectedRange)
+    }
+
+    @IBAction func authenticateTapped(_ sender: Any) {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identify yourself"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                [weak self] success, authenticateError in
+                
+                guard let strongSelf = self else { return }
+                
+                DispatchQueue.main.async {
+                    if success {
+                        strongSelf.unlockSecretMessage()
+                    } else {
+                        // failed to authenticate
+                        let ac = UIAlertController(title: "Authenticaion failed", message: "You could not be verified! Please try again!", preferredStyle: .alert)
+                        
+                        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        strongSelf.present(ac, animated: true)
+                    }
+                }
+            }
+        }
+        else {
+            // no biometry
+            let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not support this future", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(ac, animated: true)
+        }
     }
     
 }
