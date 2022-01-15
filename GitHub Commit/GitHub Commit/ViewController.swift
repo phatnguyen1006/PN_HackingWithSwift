5ae17f6a9bf34b1ba9cbcb4957acfdeadb12bb61
P <phatnguyen876@gmail.com>
2022-01-15 16:27:10 +0700

    Setup Core Data Model

--- GitHub Commit/GitHub Commit/ViewController.swift
+++ GitHub Commit/GitHub Commit/ViewController.swift
@@ -11,6 +11,7 @@ import CoreData
 class ViewController: UITableViewController {
     
     var container: NSPersistentContainer!
+    var commits = [Commit]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,8 +20,26 @@ class ViewController: UITableViewController {
         saveContext()
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return commits.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Commit", for: indexPath)
+        
+        let commit = commits[indexPath.row]
+        cell.textLabel?.text = commit.message
+        cell.detailTextLabel?.text = commit.date.description
+        
+        return cell
+    }
+    
     func networkRequest() {
-        performSelector(inBackground: #selector(fetchCommit))
+        performSelector(inBackground: #selector(fetchCommit), with: nil)
     }
     
     func sqlite() {
@@ -52,10 +71,30 @@ class ViewController: UITableViewController {
     }
     
     func configure(commit: Commit, usingJSON json: JSON) {
+        commit.sha = json["sha"].stringValue
+            commit.message = json["commit"]["message"].stringValue
+            commit.url = json["html_url"].stringValue
+
+            let formatter = ISO8601DateFormatter()
+            commit.date = formatter.date(from: json["commit"]["committer"]["date"].stringValue) ?? Date()
+
+    }
+    
+    func loadSavedData() {
+        let request = Commit.fetchRequest()
+        let sort = NSSortDescriptor(key: "date", ascending: false)
+        request.sortDescriptors = [sort]
+        
+        do {
+            commits = try container.viewContext.fetch(request)
+            tableView.reloadData()
+        } catch {
+            print("Fetch failed")
+        }
         
     }
     
-    @objc func fetchCommit() {
+    @objc func fetchCommit( ) {
         if let data = try? String(contentsOf: URL(string: "https://api.github.com/repos/apple/swift/commits?per_page=100")!) {
             
             let jsonCommits = JSON(parseJSON: data)
