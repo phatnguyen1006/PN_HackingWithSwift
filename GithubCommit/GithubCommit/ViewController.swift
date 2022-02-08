2cf632f1b81b1905ddcb736922ac6508b303379c
P <19521998@gm.uit.edu.vn>
2022-02-08 13:51:05 +0700

    add author

--- GithubCommit/GithubCommit/ViewController.swift
+++ GithubCommit/GithubCommit/ViewController.swift
@@ -35,9 +35,16 @@ class ViewController: UITableViewController {
         
         let commit = commits[indexPath.row]
         cell.textLabel?.text = commit.message
-        cell.detailTextLabel?.text = commit.date.description
+        cell.detailTextLabel?.text = "By \(commit.author.name) on \(commit.date.description)"
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.detailItem = commits[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
     
     func navBar() {
@@ -110,6 +117,27 @@ class ViewController: UITableViewController {
         
         let formatter = ISO8601DateFormatter()
         commit.date = formatter.date(from: json["commit"]["committer"]["date"].stringValue) ?? Date()
+        
+        var commitAuthor: Author!
+        
+        let authorRequest = Author.createFetchRequest()
+        authorRequest.predicate = NSPredicate(format: "name == %@", json["commit"]["committer"]["name"].stringValue)
+        
+        if let authors = try? container.viewContext.fetch(authorRequest) {
+            if authors.count > 0 {
+                commitAuthor = authors[0]
+            }
+        }
+        
+        if commitAuthor == nil {
+            // we didn't find saved author - create new one !
+            let author = Author(context: container.viewContext)
+            author.name = json["commit"]["committer"]["name"].stringValue
+            author.email = json["commit"]["committer"]["email"].stringValue
+            commitAuthor = author
+        }
+        
+        commit.author = commitAuthor
     }
     
         
@@ -154,6 +182,11 @@ class ViewController: UITableViewController {
             self.loadSavedData()
         }))
         // 4
+        ac.addAction(UIAlertAction(title: "Show only Durian commits", style: .default, handler: { [unowned self] _ in
+            self.commitPredicate = NSPredicate(format: "author.name == 'Joe Groff'")
+            self.loadSavedData()
+        }))
+        // 5
         ac.addAction(UIAlertAction(title: "Show all commits", style: .default, handler: { [unowned self] _ in
             self.commitPredicate = nil
             self.loadSavedData()
