129e33930d4e68f23bd587aa15ddf8894c462c1a
P <19521998@gm.uit.edu.vn>
2022-02-12 15:29:46 +0700

    Multibrowser

---
+++ Multibrowser/Multibrowser/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Multibrowser
+//
+//  Created by Phát Nguyễn on 11/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var addressBar: UITextField!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
